39733912a4066e9a6448110004c82e7e07adbef0
Kuziwa Anesu <ka.mandimutsira@gmail.com>
2023-02-17 11:06:55 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+/* A program that prints all single digit numbers of base 10 starting from 0*/
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 48;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
