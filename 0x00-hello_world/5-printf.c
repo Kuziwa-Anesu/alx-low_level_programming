6b13b80bea79ea6f5f3a31df3ea9d5dfc0e208cb
Kuziwa Anesu <ka.mandimutsira@gmail.com>
2023-02-16 19:22:18 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/* A program in C to print a string */
+#include <stdio.h>
+/**
+ * main - uses printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
