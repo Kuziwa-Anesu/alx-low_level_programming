cc9333de2d9c22b92036509db458041d23760cb6
Kuziwa Anesu <ka.mandimutsira@gmail.com>
2023-02-16 19:10:44 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/* A to print a string */
+#include <stdio.h>
+/**
+ *main - puts "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0
+*/
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
