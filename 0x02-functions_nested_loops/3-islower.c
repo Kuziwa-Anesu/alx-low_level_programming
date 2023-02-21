2064b60d71dda7be40acdca1ac07a20f16f73177
Kuziwa Anesu <ka.mandimutsira@gmail.com>
2023-02-21 01:51:18 -0800

    Taks 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+/* A function to check whether a character is lower-case */
+/**
+ *_islower - checks for lowercase character
+ *@c: The number to be checked
+ *
+ * Return: 1 if c is lowercase. 0 otherwise.
+ */
+int _islower(int c)
+{
+	if (97 <= c && 122 <= c)
+		return (1);
+	else
+		return (0);
+}
