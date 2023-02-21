1cecf4e3f4ba9b425cfa53481367b715b74a92ed
Kuziwa Anesu <ka.mandimutsira@gmail.com>
2023-02-21 02:12:55 -0800

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+/* A function to check for an alphabetic character */
+/**
+ *_isalpha - checks for alphabetic character
+ *@c: The character to be checked
+ *
+ * Return: 1 if c is alphabetic. 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if (97 <= c && 122 <= c || 65 <= c && 90 >= c)
+		return (1);
+	else
+		return (0);
+}
