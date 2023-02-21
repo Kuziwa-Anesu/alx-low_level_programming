6b54adc1d0ff57549860a7915b3f2d176fb1af65
Kuziwa Anesu <ka.mandimutsira@gmail.com>
2023-02-21 14:49:32 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
+/*A program using _putchar*/
+#include "main.h"
 /**
- * main - entry poinnt
- * Description: use putchar() to print
+ * main - entry point
+ * _putchar - prints a character
+ * Description: use _putchar() to print
  *
  * Return: 0
  */
 int main(void)
 {
-	putchar(95);
-	putchar(112);
-	putchar(117);
-	putchar(116);
-	putchar(99);
-	putchar(104);
-	putchar(97);
-	putchar(114);
-	putchar(10);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
 	return (0);
 }
