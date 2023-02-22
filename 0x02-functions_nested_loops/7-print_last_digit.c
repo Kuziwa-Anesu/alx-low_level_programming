7efdc678902e9a6f2d5ada2e1879ecf8b16658fc
Kuziwa Anesu <ka.mandimutsira@gmail.com>
2023-02-21 18:07:28 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+/**
+ * print_last_digit - prints last digit of a number
+ * @r: number to be checked
+ *
+ * Return: Returns the value of the last digit
+ */
+int print_last_digit(int r)
+{
+	r = r % 10;
+	return (r);
+}
