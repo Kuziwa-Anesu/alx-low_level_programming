3de9f4b25b9cc4b25e54dcd7631da080a4c52bbc
Kuziwa Anesu <ka.mandimutsira@gmail.com>
2023-02-21 18:21:52 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,6 +6,16 @@
  */
 int print_last_digit(int r)
 {
-	r = r % 10;
-	return (r);
+	if (r > 0)
+	{
+		r = r % 10;
+		return (r);
+	}
+	if (r < 0)
+	{
+		r = (r * -1) % 10;
+		return (r);
+	}
+	else
+		return (r);
 }
