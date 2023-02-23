899e77e16851eba6fba357492fae1e8455fcc125
Kuziwa Anesu <ka.mandimutsira@gmail.com>
2023-02-22 19:51:58 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - adds two integers
+ * @x: second integer to be added
+ * @y: First integer to be added
+ *
+ *Return: x+y
+ */
+int add(int x, int y)
+{
+	int added;
+
+	added = (x + y);
+
+	return (added);
+}
