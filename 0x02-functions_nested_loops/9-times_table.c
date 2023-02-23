02578de16dcf2e28290bc8d2e9f4193574238386
Kuziwa Anesu <ka.mandimutsira@gmail.com>
2023-02-22 19:35:01 -0800

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			if ((x * y) < 10 && y != 9)
+			{
+				_putchar(48 + (y * x));
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if ((x * y) > 10)
+			{
+				if (y == 9 || x < 2)
+				{
+					_putchar(48 + ((x * y) / 10));
+					_putchar(48 + ((x * y) % 10));
+				}
+				else
+				{
+					_putchar(48 + ((x * y) / 10));
+					_putchar(48 + ((x * y) % 10));
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+	}
+}
+
