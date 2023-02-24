c76e3577af1d91eb73035015b3e70c544c2dcc7a
Kuziwa Anesu <ka.mandimutsira@gmail.com>
2023-02-24 06:13:13 -0800

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ *
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size; j > i; j--)
+		{
+			_putchar('.');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar(10);
+	}
+}
